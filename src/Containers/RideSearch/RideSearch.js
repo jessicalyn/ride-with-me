49507e2718c4667153239873b3cc7946c3fc753f
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-20 16:05:35 -0600

    Add inputs for search form

--- src/Containers/RideSearch/RideSearch.js
+++ src/Containers/RideSearch/RideSearch.js
@@ -7,6 +7,21 @@ export class RideSearch extends Component {
     return(
       <div className="containers ride-search-container">
         <h3>Find a Ride</h3>
+        <form className="search-form">
+          <div className="search-divs">
+            <label>Start Location</label>
+            <input></input>
+          </div>
+          <div className="search-divs">
+            <label>End Location</label>
+            <input></input>
+          </div>
+          <div className="search-divs">
+            <label>Start Date</label>
+            <input></input>
+          </div>
+          <button>Search</button>
+        </form>
         <SearchResults />
       </div>
     )
