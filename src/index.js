6f744fba51016e7aeb15df1582bc30c9aa8ea331
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-19 20:49:27 -0600

    Install router and thunks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import { rootReducer } from './Reducers';
 import './index.scss';
 import App from './Containers/App/App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
+const router = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
-serviceWorker.unregister();
+ReactDOM.render(router, document.getElementById('root'));
