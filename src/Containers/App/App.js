910cac3af0b95dd8da949a074deac46612cacf0f
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-19 21:42:01 -0600

    Setup RideSearch container

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { RideSearch } from '../RideSearch/RideSearch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ride With Me</h1>
+        <button>Messages</button>
+        <button>My Rides</button>
+        <button>Profile</button>
       </header>
+      <RideSearch />
     </div>
   );
 }
