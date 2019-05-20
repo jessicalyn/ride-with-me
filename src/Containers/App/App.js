7a324fa0fcabf596b94e7eab9ae5971ef6a54bcf
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-20 15:17:30 -0600

    Create Profile component

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import { RideSearch } from '../RideSearch/RideSearch';
 import { CreateRide } from '../CreateRide/CreateRide';
 import { MyRides } from '../MyRides/MyRides';
+import { Profile } from '../Profile/Profile'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ride With Me</h1>
+        <Link to='/' className="home-button"><h1>Ride With Me</h1></Link>
         <div className="header-buttons">
-          <button>Messages</button>
-          <button>My Rides</button>
-          <button>Profile</button>
+          <Link to='/myrides' className="nav-buttons">My Rides</Link>
+          <Link to='/profile' className="nav-buttons">Profile</Link>
         </div>
       </header>
       <Route exact path='/' component={ RideSearch } />
       <Route exact path='/' component={ CreateRide } />
       <Route path='/myrides' component={ MyRides } />
+      <Route path='/profile' component={ Profile } />
     </div>
   );
 }
---
+++ src/Containers/Profile/Profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class Profile extends Component {
+
+  render() {
+    return(
+      <div className="containers ride-search-container">
+        <h3>My Profile</h3>
+        <h4>Name</h4>
+        <h4>Email</h4>
+        <h4>Phone</h4>
+        <h4>Password</h4>
+      </div>
+    )
+  }
+}
