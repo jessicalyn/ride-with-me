9431025885f56d103a30b26fdae64b1fab36cc56
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-20 16:10:55 -0600

    Add dropdown to find a ride form

--- src/Containers/RideSearch/RideSearch.js
+++ src/Containers/RideSearch/RideSearch.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import { SearchResults } from '../../components/SearchResults/SearchResults';
 
 export class RideSearch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      start_location: "",
+      end_location: "",
+      start_date: ""
+    }
+  }
   
   render() {
+    const { start_location } = this.state
     return(
       <div className="containers ride-search-container">
         <h3>Find a Ride</h3>
         <form className="search-form">
           <div className="search-divs">
             <label>Start Location</label>
-            <input></input>
+            <select className="search-drop-down" value={start_location} name="search_start_location" onChange={this.handleChange}>
+              <option value="0" disable="true" select="true" default>Select a City</option>
+            </select>
           </div>
           <div className="search-divs">
             <label>End Location</label>
