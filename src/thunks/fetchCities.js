afcc2450502277a111a3137bec51aa6b0b1aca5d
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-21 15:38:03 -0600

    Setup thunk and fetchOptionsCreator for fetching cities

---
+++ src/thunks/fetchCities.js
@@ -0,0 +1,20 @@
+import { hasError } from '../actions';
+import { fetchOptionsCreator } from '../utils/fetchOptionsCreator';
+
+export const fetchCities = () => {
+  return async (dispatch) => {
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/v1/`
+    try {
+      const body = whatevertheyneed
+      const options = fetchOptionsCreator('POST', body)
+      const response = await fetch(url, options)
+      if (!response.ok) {
+        throw Error (response.statusText)
+      }
+      const result = await response.json()
+      dispatch(storeCities(cities))
+    } catch(error) {
+      dispatch(hasError(error.message))
+    }
+  }
+}
