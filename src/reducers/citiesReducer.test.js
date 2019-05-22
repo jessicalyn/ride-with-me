2e6aee122014407f05f6abf5ad645adf22705931
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-22 16:07:52 -0600

    Update ignored files for testing coverage

--- src/reducers/citiesReducer.test.js
+++ src/reducers/citiesReducer.test.js
@@ -2,21 +2,21 @@ import { citiesReducer } from './citiesReducer';
 import * as actions from '../actions';
 
 describe('citiesReducer', () => {
-  const mockState = []
+  const state = []
 
   it('should return state by default', () => {
     const action = {}
 
-    const result = citiesReducer(mockState, action)
+    const result = citiesReducer(state, action)
 
-    expect(result).toEqual(mockState)
+    expect(result).toEqual(state)
   })
 
   it('should return state as an array of cities', () => {
     const mockCities = ["Denver", "Chicago", "San Francisco"]
     const action = actions.storeCities(mockCities)
 
-    const result = citiesReducer(mockState, action)
+    const result = citiesReducer(state, action)
 
     expect(result).toEqual(mockCities)
   })
